09cc40df30460dc91a579129312955d864928a7d
Eric Wilson <wilson.eric.n@gmail.com>
2015-09-12 16:30:04 -0400

    better stats

--- dice.py
+++ dice.py
@@ -1,6 +1,7 @@
 from random import randint
 from collections import Counter
 
+
 def roll(number, hit_level):
     rolls = [Roll(hit_level) for _ in range(number)]
     return Results(rolls)
@@ -28,14 +29,30 @@ class Results(object):
         return "Hits: %s\n%s" % (self.count, roll_str if len(self.results) < 25 else '')
 
 
-all_results = Counter()
+all_results = {}
 
 
-def record(results):
+class Stat(object):
+
+    def __init__(self, player):
+        self.c = Counter()
+        self.player = player
+
+    def update(self, results):
+        self.c.update([r.num for r in results])
+
+    def stat_line(self):
+        stuff = [(n, self.c[n]/sum(self.c.values())) for n in range(1,7)]
+        return self.player + ') ' + ', '.join(["%d: %4.1f%%" % (t[0], 100*t[1]) for t in stuff])
+
+
+def record(results, player):
     global all_results
-    all_results.update([r.num for r in results])
+    if player not in all_results:
+        all_results[player] = Stat(player)
+    all_results[player].update(results)
 
 
 def stats():
     global all_results
-    return all_results
+    return [all_results[k].stat_line() for k in all_results]
--- main.py
+++ main.py
@@ -8,23 +8,32 @@ class DiceShell(cmd.Cmd):
     intro = """Welcome to simple dice. Type help or ? to list commands.\n"""
     prompt = '>> '
 
-    def do_roll(self, arg):
+    def do_x_roll(self, arg):
+        """Roll a number of dice with a hit level. Example: ROLL 10 3"""
+        self._do_roll(arg, 'x')
+
+    def do_a_roll(self, arg):
+        """Roll a number of dice with a hit level. Example: ROLL 10 3"""
+        self._do_roll(arg, 'a')
+
+    def _do_roll(self, arg, player):
         """Roll a number of dice with a hit level. Example: ROLL 10 3"""
         try:
             params = parse(arg)
             assert len(params) == 2
             results = roll(*params)
-            record(results.results)
+            record(results.results, player)
             print(results)
         except ValueError:
             print("Unable to parse integer arguments")
-            print(getdoc(self.do_roll))
+            print(getdoc(self._do_roll))
         except AssertionError:
             print("Incorrect number of arguments")
-            print(getdoc(self.do_roll))
+            print(getdoc(self._do_roll))
 
     def do_stats(self, arg):
-        print(stats())
+        stat_lines = '\n'.join(line for line in stats())
+        print(stat_lines)
 
     def do_exit(self, arg):
         """Say goodbye and exit"""
@@ -35,7 +44,7 @@ class DiceShell(cmd.Cmd):
 def parse(arg):
     """Convert a series of zero or more numbers to an argument tuple"""
     args = arg.split()
-    return [int(s) for s in args]
+    return int(args[0]), int(args[1])
 
 if __name__ == '__main__':
     DiceShell().cmdloop()
